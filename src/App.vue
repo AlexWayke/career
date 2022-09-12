8c9aff9ea8227621e5e956d2bd8e772dbad5e24e
AlexWayke <yuhcha2@yandex.ru>
2022-09-12 23:11:07 +0400

    working enviroment

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Modal />
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  min-height: 100%;
+  position: relative;
+}
+// Null styles ====================
+@import "/css/style";
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="modal-backdrop">
+        <div class="modal-content">
+            <p>Modal content</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.modal-backdrop{
+    position: fixed;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.5);
+}
+.modal-content{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+    text-align: center;
+}
+</style>
