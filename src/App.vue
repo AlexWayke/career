7723be8117bf2efb96c5b2bed019a27df6a30c86
AlexWayke <yuhcha2@yandex.ru>
2022-09-14 23:01:43 +0400

    form vue
    base principals

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,14 @@
 <template>
-  <h1>{{ title }}</h1>
-  <teleport to=".modals" v-if="showModal">
-    <Modal theme="sale" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">sign up now</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Lorem</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minima.</p>
-    </Modal>
-  </teleport>
-  <teleport to=".modals" v-if="showModalTwo">
-    <Modal theme="sale" @close="toggleModalTwo">
-      <template v-slot:links>
-        <a href="#">sign up now</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Lorem 2</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </Modal>
-  </teleport>
-  <button @click="toggleModal" class="modal-button">Open modal</button>
-  <button @click="toggleModalTwo" class="modal-button">Open another modal</button>
+  <SIgnUpForm />
 </template>
 
 <script>
-import Modal from '@/components/Modal.vue'
+import SIgnUpForm from './components/SIgnUpForm.vue';
 
 export default {
   name: 'App',
-  components: { Modal },
+  components: { SIgnUpForm },
   data() {
-    return {
-      second: 'Lorem ipsum',
-      showModal: false,
-      showModalTwo: false
-    }
-  },
-  methods: {
-    toggleModal() {
-      this.showModal = !this.showModal
-    },
-    toggleModalTwo() {
-      this.showModalTwo = !this.showModalTwo
-    }
   }
 }
 </script>
